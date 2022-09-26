ad8c7420b7de799b2ef1a056f215a53d29f1d07b
Artur Moraes <artur.gomoraes@gmail.com>
2022-09-26 17:32:12 -0300

    Slider com gradient

--- src/components/Grades/index.tsx
+++ src/components/Grades/index.tsx
@@ -4,8 +4,8 @@ export function Grades() {
   return (
     <div className="w-full">
       <Slider.Root defaultValue={[0]} min={0} max={100} step={10} className="relative flex items-center h-5">
-        <Slider.Track className="bg-[#e5e5e5] w-full h-3">
-          <Slider.Range className="bg-firedev-linear" />
+        <Slider.Track className="bg-[#e5e5e5] w-full h-3 relative">
+          <Slider.Range className="h-full rounded-full bg-firedev-linear absolute" />
         </Slider.Track>
         <Slider.Thumb className="bg-black w-5 h-5 block" />
       </Slider.Root>
